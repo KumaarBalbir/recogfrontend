95b40043b4e8ddc886448c6e6647535504e6f60a
Kumaar-Balbir <prasadbalbir1056@gmail.com>
2022-05-29 13:43:47 +0530

    added netlify.toml

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,10 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri:
-    !process.env.NODE_ENV || process.env.NODE_ENV === "development"
-      ? "http://localhost:4000/"
-      : "https://floating-anchorage-67010.herokuapp.com/",
+  uri:"https://floating-anchorage-67010.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
